7b2e80c4b5f42b5c3a05d99e507989ca83b1cc12
prong-prong <dazeleb@gmail.com>
2026-05-21 22:44:20 +0900

    add PWA support

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'cooking-diary-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/data.js',
+  '/ingredients.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
